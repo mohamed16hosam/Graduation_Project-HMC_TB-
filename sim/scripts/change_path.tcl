fccdd7dc6dedf3f362af02e2862a8f1e81493a16
mohamed16hosam <82730843+mohamed16hosam@users.noreply.github.com>
2023-10-17 06:39:12 +0300

    Add files via upload

---
+++ sim/scripts/change_path.tcl
@@ -0,0 +1,57 @@
+# to run:
+#1. open Tclsh86
+#2. cd current path
+#3. source change_path.tcl
+
+puts "\n"
+#Title
+puts "**** Change Files Path ****"
+
+#open the file
+set fh [open dut.f r+]
+
+#read the data of the file
+set dut [read $fh]
+puts "\nold file:\n"
+puts $dut
+
+#create a second file 
+set fhnew [open dut_new.f w+]
+
+#substitule path
+regsub -all "D:/Ali/College/" $dut "G:/My_Studies/Graduation_Project/HMC_Testbench/TB/" dut
+
+#print file content
+puts "\nnew file:\n"
+puts $dut
+puts $fhnew $dut
+
+#closing the files
+close $fh
+close $fhnew
+#**************************************************************#
+#**************************************************************#
+puts "\n"
+#open the file
+set fh [open tb.f r+]
+
+#read the data of the file
+set tb [read $fh]
+puts "\nold file:\n"
+puts $tb
+
+#create a second file 
+set fhnew [open tb_new.f w+]
+
+#substitule path
+regsub -all "D:/Ali/College/" $tb "G:/My_Studies/Graduation_Project/HMC_Testbench/TB/" tb
+
+
+#print file content
+puts "\nnew file:\n"
+puts $tb
+puts $fhnew $tb
+
+#closing the files
+close $fh
+close $fhnew
